a57c1df10764b2eb71aa480229b47e63ecd18658
Ken Guan <21guanr@berkeley.edu>
2020-07-18 05:15:49 +0000

    implement simple gradient-based attack on weighted avg ensemble

--- test_mnist_ensemble.py
+++ test_mnist_ensemble.py
@@ -12,6 +12,7 @@ import yaml
 from adv.dataset_utils import load_mnist_all
 from adv.mnist_model import BasicModel, BatchNormModel, EnsembleModel
 from adv.utils import get_logger, trades_loss, classify, classify_ensemble, get_acc, get_logger, quantize
+from adv.pgd_attack import PGDAttack
 
 with open('test_mnist.yml', 'r') as stream:
     config = yaml.safe_load(stream)
@@ -20,6 +21,7 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = config['meta']['gpu_id']
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+batch_size = config['test']['batch_size']
 
 def load_ensemble_model(name):
     path = os.path.join('saved_models', name)
@@ -30,7 +32,7 @@ def load_ensemble_model(name):
         net = net.eval().to(device)
         net.load_state_dict(torch.load(os.path.join(path, file)))
         single_models.append(net)
-    return EnsembleModel(single_models)
+    return EnsembleModel(single_models).to(device)
 
 def main():
     (x_train, y_train), (_, _), (x_test, y_test) = load_mnist_all(
@@ -53,6 +55,14 @@ def main():
     acc = get_acc(y_pred, y_test[:num_test_samples])
 
     log.info('Clean acc: %.4f', acc)
+
+    log.info('Starting ensemble PGD attack...')
+    attack = PGDAttack(ensemble, x_train, y_train)
+    x_adv = attack(x_test[:num_test_samples], y_test[:num_test_samples], batch_size=batch_size, **config['pgd'])
+    y_pred = classify_ensemble(ensemble, x_adv, num_classes=num_classes)
+    adv_acc = get_acc(y_pred, y_test[:num_test_samples])
+
+    log.info('Adv acc: %.4f', adv_acc)
 
 
 if __name__ == '__main__':
